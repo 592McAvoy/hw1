e4ba3bbe56d9b26c875acf49c4584b2045e4cc1e
592McAvoy <1245709462@qq.com>
2018-03-01 22:20:16 +0800

    写了一点点

--- myapp/src/main/java/com/mycompany/app/App.java
+++ myapp/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,11 @@ import java.io.*;
  */
 public class App 
 {
-	public static Set<String> words = new HashSet<String>();
+	public static Set<String> dict = new HashSet<String>();//字典
+	public static Set<String> record = new HashSet<String>();//记录替换后的新单词，避免出现无限循环
+	public static Stack<String> result = new Stack<String>();;	//WordLadder的结果
+	public static Queue<Stack<String>> ladder = new LinkedList<Stack<String>>();;//用于存储ladder的队列
+	
 	
     public static void main( String[] args )
     {
@@ -21,12 +25,52 @@ public class App
     		boolean read = setDict("D:\\罗宇辰\\作业\\大二下\\软工后端\\hw1\\myapp\\src"
     				                + "\\main\\java\\com\\mycompany\\app\\"+filename);//设置字典
     		if (read) {
-    			System.out.println(words);
+    			System.out.println(dict);
     			break;
     			}
     		System.out.println("Unable to open that file.  Try again.\n\n");
     	}
-    	System.out.println( "Done!");
+    	
+    	String word1;//终点单词
+    	String word2;//起点单词
+    	
+    	//从用户处得到起点和终点的单词
+    	while (true) {
+    		
+    			System.out.println("Word #1 (or Enter to quit): ");
+    			word1 = in.nextLine().toLowerCase();
+    			if (word1.equals(""))break;
+    			
+    			System.out.println( "Word #2 (or Enter to quit): ");
+    			word2 = in.nextLine().toLowerCase();
+    			//排查不规范输入
+    			if (word2.equals(""))break;
+    			if (word1.equals(word2)) {
+    				System.out.println("The two words must be different.");
+    				continue;
+    			}
+    			//初始化ladder队列
+    			Stack<String> ss = new Stack<String>();
+    			ss.push(word1);
+    			ladder.offer(ss);
+
+    			//处理查找结果
+    			/*boolean _result = find_ladder(word2);
+    			if (_result) {
+    				System.out.println( "A ladder from " + word2 + " back to " + word1 + " :\n");
+    				while (!result.empty()) {
+    					System.out.println( result.top() + " ");
+    					result.pop();
+    				}
+    				System.out.println("\n\n");
+    			}
+    			else
+    				System.out.println( "No word ladder found from " + word2 + " back to " + word1 + " .\n\n");
+    				*/
+    		
+    		
+    	}
+    	System.out.println( "Have a nice day!");
     }
     
     public static boolean setDict(String fileName) {  
@@ -34,14 +78,10 @@ public class App
         BufferedReader reader = null;  
         try {   
             reader = new BufferedReader(new FileReader(file));  
-            String tempString = null;  
-            int line = 1;  
+            String tempString = null;   
             // 一次读入一行，直到读入null为文件结束  
-            while ((tempString = reader.readLine()) != null ) {  
-                // 显示行号  
-            	words.add(tempString);
-                //System.out.println("line " + line + ": " + tempString);  
-                line++;  
+            while ((tempString = reader.readLine()) != null ) {    
+            	dict.add(tempString);                 
             }  
             reader.close();  
             return true;
